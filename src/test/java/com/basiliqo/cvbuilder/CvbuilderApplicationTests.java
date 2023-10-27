aa7784e432d6175bd3b403a09f3369912498c9a4
Viktor Bakulin <bakulinvm@gmail.com>
2023-10-27 23:08:11 +0400

    init: initial commit

---
+++ src/test/java/com/basiliqo/cvbuilder/CvbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basiliqo.cvbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvbuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
